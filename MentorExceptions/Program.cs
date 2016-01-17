4ce6d6a5473f47b747c4d09bf69dd75c43ebf4c7
Vitali Pantsialei <bitvitalby@gmail.com>
2016-01-17 16:42:01 +0300

    Console project empty string exception

---
+++ MentorExceptions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NLog;
+
+namespace MentorExceptions
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter '#' when you want to exit");
+            string inputString;
+            do
+            {
+                inputString = Console.ReadLine();
+                if (inputString == "#")
+                    break;
+                try
+                {
+                    Console.WriteLine(inputString[0]);
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    logger.Info("User has entered empty string");
+                    Console.WriteLine("Empty string was entered");
+                }
+            }
+            while (inputString != "#");
+        }
+    }
+}
